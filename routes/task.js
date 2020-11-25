03575cb7912f5bae61536946161e96803d2a0e26
Diego <diegotozetto@gmail.com>
2020-11-25 02:43:45 -0300

    Add requisição para atualizar task e modificação no retorno

--- routes/task.js
+++ routes/task.js
@@ -10,23 +10,41 @@ router.get('/', (req, res) => {
     Task.find().sort({name: 'asc'}).then((tasks) => {
         res.status(200).json(tasks);
     }).catch(() => {
-        res.status(500).json({error: "Falha ao processar requisição. Erro ao buscar tarefa no Database."});
+        res.status(500).json({"message": "Falha ao processar requisição. Erro ao buscar tarefa no Database."});
     });
 });
 
 //Adicionar Tarefa
 router.post('/', (req, res) => {
     if (Object.keys(req.body).length === 0) {
-        res.status(400).json({error: "Falha ao processar requisição. Parâmetros Inválidos."});
+        res.status(400).json({"message": "Falha ao processar requisição. Parâmetros Inválidos."});
     } else {        
         new Task({
             text: req.body.text  
         }).save().then(() => {
-            res.status(201).json({error: "Tarefa adicionada com sucesso."});
+            res.status(201).json({"message": "Tarefa adicionada com sucesso."});
         }).catch(() => {
-            res.status(500).json({error: "Falha ao processar requisição. Erro ao adicionar tarefa no Database."});
+            res.status(500).json({"message": "Falha ao processar requisição. Erro ao adicionar tarefa no Database."});
         });           
     }   
+});
+
+//Atualizar Tarefa
+router.put('/:id', (req, res) => {
+
+    Task.findOne({_id: req.params.id}).then((task) => {
+        
+        task.marked = req.body.marked
+
+        task.save().then(() => {
+            res.status(201).json({"message": "Tarefa atualizada com sucesso."});
+        }).catch((error) => {
+            res.status(500).json({"message": "Falha ao processar requisição. Erro ao atualizar tarefa no Database."});
+        });
+         
+    }).catch((error) => {
+        res.status(500).json({"message": "Falha ao processar requisição. Erro ao atualizar tarefa no Database."});
+    });
 });
 
 //Remover Tarefa
