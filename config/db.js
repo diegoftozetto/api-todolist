5c8a67dce32dd1ceff6b845ec9b20052b7870b7b
Diego <diegotozetto@gmail.com>
2020-12-16 23:47:15 -0300

    Atualiza banco de dados

--- config/db.js
+++ config/db.js
@@ -1,6 +1,7 @@
   
 if(process.env.NODE_ENV == "production"){
-    module.exports = {mongoURI: "mongodb://admin:admin123@ds037067.mlab.com:37067/db-todolist"}
+    //module.exports = {mongoURI: "mongodb://admin:admin123@ds037067.mlab.com:37067/db-todolist"}
+    module.exports = {mongoURI: "mongodb+srv://admin:admin@cluster0.tzavo.mongodb.net/db-todolist?retryWrites=true&w=majority"}
 }
 else{
     module.exports = {mongoURI: "mongodb://localhost/db-todolist"}
